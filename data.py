459bf825a8c7658f975413bd81b2830dbe472bc0
Beilong <bt132@duke.edu>
2024-11-02 10:21:47 +0800

    fix importing data error

--- data.py
+++ data.py
@@ -19,9 +19,6 @@ def import_data(data: str, root=".") -> Tuple[np.ndarray, np.ndarray]:
     P = np.hstack([normalize(P[:, [i]], norm="l1", axis=0) for i in range(P.shape[1])])
     Z = np.where(P > 0, 1, 0)
     Z = np.hstack([normalize(Z[:, [i]], norm="l1", axis=0) for i in range(Z.shape[1])])
-    if data == "Soil_Vivo":
-        Z, P = shuffle(Z, P)
-        Z, P = np.split(Z, [int(0.1 * len(Z))]), np.split(P, [int(0.1 * len(P))])
     Z = np.transpose(Z)
     P = np.transpose(P)
     return Z, P
